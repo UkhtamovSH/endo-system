ad86a98add50e339d975db1fa827984d2c07e2bf
UkhtamovSH <uktamovshokhrukh@gmail.com>
2022-07-08 15:27:18 +0500

    project in proccess

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// **************navigation active function start**************
+$(function () {
+  var current = location.pathname;
+  $('.navbar-nav .nav-link').each(function () {
+    var $this = $(this);
+    if ($this.attr('href') === current.substring(1)) {
+      $this.addClass('active');
+    }
+  });
+});
+// **************navigation active function end**************
+
+window.onscroll = function () {
+  let t = document.getElementById('textt');
+  if (document.documentElement.scrollTop) {
+    t.classList.add('mystyle');
+  } else {
+    t.classList.remove('mystyle');
+  }
+};
