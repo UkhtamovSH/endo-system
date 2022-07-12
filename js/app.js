854c02dc0fc94961be001da7905de8325808fdd4
UkhtamovSH <uktamovshokhrukh@gmail.com>
2022-07-12 22:43:05 +0500

    project in process 5

--- js/app.js
+++ js/app.js
@@ -1,30 +1,25 @@
 // **************navigation active function start**************
 $(function () {
   var current = location.pathname;
-  $('.navbar-nav .nav-link').each(function () {
+  $('.navigation-two__flex-right .nav_link').each(function () {
     var $this = $(this);
     if ($this.attr('href') === current.substring(1)) {
       $this.addClass('active');
     }
   });
-  $('.catalog-flex__left-sub a').each(function () {
-    var $this = $(this);
-    console.log(
-      $this.attr('href') + '?catalog=' + 'catalog1' ===
-        current + '?catalog=' + 'catalog1'
-    );
-    // console.log(current.substring(1) + '?catalog=' + 'catalog1');
-    // console.log($this.attr('href') === current + '?catalog=' + 'catalog1');
-    if (
-      $this.attr('href') + '?catalog=' + 'catalog1' ===
-      current.substring(1) + '?catalog=' + 'catalog1'
-    ) {
-      console.log(true);
-      $this.addClass('active');
-    }
-  });
 });
 // **************navigation active function end**************
+
+// **************navigation scroll function start**************
+$(window).on('scroll', function () {
+  if ($(window).scrollTop()) {
+    $('nav').addClass('scrollnav');
+  } else {
+    $('nav').removeClass('scrollnav');
+  }
+});
+
+// **************navigation scroll function end**************
 
 // window.onscroll = function () {
 //   let t = document.getElementById('textt');
