7c0e511080184ba8d375730db05fc77d4b114473
UkhtamovSH <uktamovshokhrukh@gmail.com>
2022-07-15 16:58:39 +0500

    project in process 19

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // **************navigation active function start**************
 $(function () {
   var current = location.pathname;
-  $('.navigation-two__flex-right .nav_link').each(function () {
+  $('.nav_link').each(function () {
     var $this = $(this);
     if ($this.attr('href') === current.substring(1)) {
       $this.addClass('active');
@@ -32,3 +32,25 @@ function closeNav() {
   document.body.style.overflow = 'visible';
 }
 // **************mobile navigation end**************
+
+// **************AOS animation start**************
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 20, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 2, // offset (in px) from the original trigger point
+  duration: 1500, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom',
+});
+// **************AOS animation end**************
